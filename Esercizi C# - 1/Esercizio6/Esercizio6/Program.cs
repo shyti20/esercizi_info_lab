428b65821262120bf618845d4ac07449f5871672
Shyti Franko <149884850+shyti20@users.noreply.github.com>
2024-01-29 09:56:12 +0100

    Add files via upload

---
+++ Esercizi C# - 1/Esercizio6/Esercizio6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Esercizi6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char l;
+            
+            Console.WriteLine("Inserisci una lettera: ");
+            l = Convert.ToChar(Console.ReadLine());
+            
+        }
+    }
+}
