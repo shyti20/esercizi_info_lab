82e3c56cefea43465ec9f39833083d59b95fe77e
Shyti Franko <149884850+shyti20@users.noreply.github.com>
2024-01-31 23:43:08 +0100

    Add files via upload

---
+++ C#/Esercizio01/Esercizio1/Program.cs
@@ -0,0 +1,28 @@
+﻿//1. Scrivere un programma che, dati due valori interi, calcoli e visualizzi la loro somma.
+//Shyti Franko 4AI
+namespace Esercizio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            do
+            {
+                Console.WriteLine("Inserisci il primo valore: ");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a <= 0);
+
+            do
+            {
+                Console.WriteLine("Inserisci il primo valore: ");
+                b = Convert.ToInt32(Console.ReadLine());
+            } while (b <= 0);
+
+            int s = a + b;
+
+            Console.WriteLine("Somma: " + s);
+
+        }
+    }
+}
---
+++ C#/Esercizio04/Esercizio4/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 4. Scrivere un programma che calcoli la media aritmetica fra numeri naturali (zero escluso) specificati
+dall’utente. La richiesta di nuovi numeri si arresta all’inserimento di uno zero.
+Shyti Franko 4AI */
+namespace Esercizio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, num = 1;
+            float media = 0;
+            while (num == 0)
+            {
+                Console.WriteLine("Inserisci l'elemento: " + i + 1);
+                num = Convert.ToInt32(Console.ReadLine());
+                media += num;
+                i++;
+            }
+
+            media /= i;
+            Console.WriteLine("Media: " + media);
+        }
+    }
+}
