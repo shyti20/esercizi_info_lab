d9e1ea61e4da1fd14af2dbfffe4c61fc19e710cd
Shyti Franko <149884850+shyti20@users.noreply.github.com>
2024-02-01 09:16:17 +0100

    Add files via upload

---
+++ C#/Esercizio08/Esercizio8/Program.cs
@@ -0,0 +1,33 @@
+﻿/*9. Scrivere un programma che verifichi se la somma dei fattoriali delle cifre di un dato numero è uguale al numero stesso.
+Shyti Franko 4AI*/
+
+namespace Esercizio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            do
+            {
+                Console.WriteLine("Inserisci un numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            } while (num < 0);
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write(Convert.ToChar('A' + j));
+                }
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    Console.Write(Convert.ToChar('A' + j));
+                }
+                Console.WriteLine();   
+            }
+        }
+    }
+}
