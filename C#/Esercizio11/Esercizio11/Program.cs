94ceb6c4dfffd126c246a2ba7273e0c86c89484a
Shyti Franko <149884850+shyti20@users.noreply.github.com>
2024-02-05 09:51:44 +0100

    Add files via upload

---
+++ C#/Esercizio11/Esercizio11/Program.cs
@@ -0,0 +1,52 @@
+﻿/*11. Lo studente realizzi un’applicazione che permetta l’acquisizione di n valori numerici compresi
+fra due dati estremi (estremi inclusi); l’applicazione deve ricavare, a scelta dell’utente finale,
+valore minimo o valore massimo.
+Shyti Franko 4AI*/
+namespace Esercizio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, min, max = 0;
+            do
+            {
+                Console.WriteLine("Inserisci il numero di elementi da inserire: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            } while (num < 0);
+
+            int[] arr = new int[num];
+
+            Console.WriteLine("Inserisci il primo valore: ");
+            arr[0] = Convert.ToInt32(Console.ReadLine());
+            
+            max = arr[0];
+            min = arr[0];
+            
+            for (int i = 1; i < arr.Length; i++)
+            {
+                Console.WriteLine("Inserisci il " + (i + 1) + " elemento: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                } else if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+
+            Console.WriteLine("max = massimo min = minimo");
+            String temp = Console.ReadLine();
+
+            if (temp == "max")
+            {
+                Console.WriteLine("Il massimo: " + max);
+            } else
+            {
+                Console.WriteLine("Il minimo: " + min);
+            }
+        }
+    }
+}
---
+++ C#/Esercizio12/Esercizio12/Program.cs
@@ -0,0 +1,51 @@
+﻿/*12. Scrivere un programma C# per inserire un elemento (in una posizione specifica) in un array
+ Shyti Franko 4AI*/
+
+using System.Reflection.PortableExecutable;
+
+namespace Esercizio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            do 
+            {
+                Console.WriteLine("Inserisci una dimensione: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n <= 0);
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Inserisci il " + (i + 1) + " elemento: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int pos;
+
+            do
+            {
+                Console.WriteLine("Inserisci una posizione: ");
+                pos = Convert.ToInt32(Console.ReadLine());
+            } while (pos <= 0 || pos > arr.Length);
+
+            for (int i = arr.Length - 1; i >= pos; i--)
+            {
+                arr[i] = arr[i - 1]; 
+            }
+
+            Console.WriteLine("Inserisci un valore: ");
+            arr[pos - 1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Stampa array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
