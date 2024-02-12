67bd1510fbf0ee1aceec6d44b5239e72a2567609
Shyti Franko <149884850+shyti20@users.noreply.github.com>
2024-02-12 09:51:59 +0100

    Add files via upload

---
+++ C#/Esercizio13/Esercizio13/Program.cs
@@ -0,0 +1,58 @@
+﻿/*13. Implementare il gioco consistente nell’indovinare 3 numeri interi generati casualmente
+dall’applicazione e compresi fra 1 e 100. Il programma dovrà indirizzare l’utente verso il numero
+giusto indicando se il numero inserito è più grande o più piccolo di quello cercato; la vittoria si
+ottiene indovinando i tre numeri in meno di 12 tentativi. Nota bene: acquisire i 3 numeri in una
+sola volta.
+Shyti Franko 4AI*/
+
+namespace Esercizio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] rnd = new int[3];
+            int num;
+
+            for (int i = 0; i < rnd.Length; i++)
+            {
+                rnd[i] = Convert.ToInt32(random.Next(1, 100 + 1));
+            }
+            int j = 0, cont = 0;
+
+            while(j < 12)
+            {
+                for (int i = 0; i < rnd.Length; i++)
+                {
+                    do
+                    {
+                        Console.WriteLine("Inserisci il " + (i + 1) + " numero compreso tra 1 e 100: ");
+                        num  = Convert.ToInt32(Console.ReadLine());
+                    } while (num <= 0 || num >= 100);
+
+                    if (num == rnd[i])
+                    {
+                        Console.WriteLine("Hai indovinato il " + (i + 1) + " numero!");
+                        cont++;
+                    } else if (num > rnd[i])
+                    {
+                        Console.WriteLine("Il numero inserito e maggiore");
+                    } else
+                    {
+                        Console.WriteLine("Il numero inserito e minore");
+                    }
+                }
+                j++;
+            }
+
+            if (j == 12)
+            {
+                Console.WriteLine("Hai esaurito i tentativi");
+            } else if (cont == 3)
+            {
+                Console.WriteLine("HAI VINTO");
+            }
+        }   
+    }
+}
---
+++ C#/Esercizio15/Esercizio15/Program.cs
@@ -0,0 +1,15 @@
+﻿/*15. Scrivere un programma per inserire una stringa nella posizione specificata in una determinata
+stringa. Es.:
+The brown fox → The quick brown fox
+Shyti Franko 4AI*/
+
+namespace Esercizio15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
